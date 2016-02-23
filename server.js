a751e0858506e5775e03c72acbb16d270175e380
schmitz1193 <schmitz1193@gmail.com>
2016-02-23 14:26:33 -0600

    passport and flash

--- server.js
+++ server.js
@@ -2,8 +2,10 @@
 
 const bodyParser = require('body-parser');
 const express = require('express');
+const flash = require('connect-flash');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 
@@ -21,14 +23,23 @@ app.use(session({
   secret: SESSION_SECRET,
   store: new RedisStore()
 }));
-app.use(userRoutes);
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.locals.title = '';
 
 app.use((req, res, next) => {
-  app.locals.user = req.session.user || { email: 'Guest' };
+  res.locals.user = req.user;
   next();
 });
+
+app.use((req, res, next) => {
+  res.locals.messages = req.flash();
+  next();
+});
+
+app.use(userRoutes);
 
 app.get('/', (req, res) => {
   res.render('index');
